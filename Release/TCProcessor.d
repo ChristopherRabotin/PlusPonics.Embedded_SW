0a6dbd79293d6624b9cfd9fdd84adcf931642440
Christopher Rabotin <chris@plusponics.com>
2013-08-31 15:39:28 +0200

    Not everything was committed. Strange.

---
+++ Release/TCProcessor.d
@@ -0,0 +1,38 @@
+TCProcessor.d TCProcessor.o: ../TCProcessor.cpp ../TCProcessor.h ../TC.h \
+  ../DigitalWriteTC.h \
+  /home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/Arduino.h \
+  /home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/binary.h \
+  /home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/WCharacter.h \
+  /home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/WString.h \
+  /home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/HardwareSerial.h \
+  /home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/Stream.h \
+  /home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/Print.h \
+  /home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/Printable.h \
+  /home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/new.h \
+  /home/chris/Software/arduino-1.0.5/hardware/arduino/variants/mega/pins_arduino.h
+
+../TCProcessor.h:
+
+../TC.h:
+
+../DigitalWriteTC.h:
+
+/home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/Arduino.h:
+
+/home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/binary.h:
+
+/home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/WCharacter.h:
+
+/home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/WString.h:
+
+/home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/HardwareSerial.h:
+
+/home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/Stream.h:
+
+/home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/Print.h:
+
+/home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/Printable.h:
+
+/home/chris/Software/arduino-1.0.5/hardware/arduino/cores/arduino/new.h:
+
+/home/chris/Software/arduino-1.0.5/hardware/arduino/variants/mega/pins_arduino.h:
